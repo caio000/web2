7167750ab684e08cd8d3e1673ff3856a235b7951
Caio de Freitas <caiof.adriano@hotmail.com>
2017-10-24 16:02:11 -0200

    Criado herança Per_class

---
+++ src/models/Library.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Library
+ *
+ */
+@Entity
+
+public class Library extends User implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Library() {}
+   
+}
--- src/models/User.java
+++ src/models/User.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
  */
 @Entity
 @Table
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public class User implements Serializable {
 
 	@Id
